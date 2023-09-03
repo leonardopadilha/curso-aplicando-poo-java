f0cf13328b3f5e371eacf463f1c5a31eaadc3c9a
leonardopadilha <leonardopadilha28@gmail.com>
2023-09-03 10:40:46 -0300

    Screenmatch - primeiro commit

---
+++ src/main/java/br/com/alura/screenmatch/modelos/Principal.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme filme = new Filme();
+        filme.setNome("300");
+        filme.setAnoDeLancamento(2019);
+        filme.exibeFichaTecnica();
+
+        filme.avalia(8);
+        filme.avalia(5);
+        filme.avalia(10);
+
+        System.out.println(filme.pegaMedia());
+        System.out.println(filme.getTotalDeAvaliacoes());
+    }
+}
