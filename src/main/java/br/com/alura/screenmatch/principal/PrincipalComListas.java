ca0ae32f007ce4e8e1ebd96f3fff3824e8b4072b
leonardopadilha <leonardopadilha28@gmail.com>
2023-09-16 09:52:16 -0300

    Utilizando a interface List

--- src/main/java/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/main/java/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -21,7 +22,7 @@ public class PrincipalComListas {
 
         Serie lost = new Serie("Lost", 2000);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoPaulo);
         lista.add(meuFilme);
         lista.add(outroFilme);
