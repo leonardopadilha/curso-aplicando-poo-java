322e19a63b62d56137539bec5e5082c9c76c0c96
leonardopadilha <leonardopadilha28@gmail.com>
2023-09-16 09:24:24 -0300

    Ordenando a lista

--- src/main/java/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/main/java/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -40,8 +41,21 @@ public class PrincipalComListas {
             if (item instanceof Filme) {
                 Filme filme = (Filme) item;
                 System.out.println("Classificação: " + filme.getClassificacao());
-            }
 
+            }
         }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+
+        System.out.println("Antes da ordenação: " + buscaPorArtista);
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação: " + buscaPorArtista);
+
+        System.out.println("Lista de títulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
     }
 }
